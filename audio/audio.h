7a000339871e45013b364ea455116379e80998b6
westonb2005 <westonb2005@333bce7a-0229-11df-9af4-1984060cff7e>
2010-01-19 06:06:59 +0000

    Updating audio code to include SFX capabilities. Will update AUDIO.txt later. I added what I think we need but we can add more if we come to it.

--- audio/audio.h
+++ audio/audio.h
@@ -10,7 +10,7 @@ using namespace std;
 class audBackground {
 	private:
 		ALuint backgroundSource;
-		string currentFolder;	//Folder to get background audio from
+		string currentFolder;	//Folder where background audio comes from
 		vector<string> files;	//Files to queue for background
 		int buffersize;		//Size of buffer queue
 		int currentFile;	//Index of files for current file
@@ -23,6 +23,22 @@ class audBackground {
 		ALint audCheck();
 		int audUpdateQueue(int play);
 		int audPlay();
+		int audStop();
+};
+
+class audSFX {
+	private:
+		ALuint *sfxSource;
+		ALuint *sfxBuffer;
+		vector<string> files;
+		int audGetFilesOfType(string folder, string type);
+	public:
+		audSFX();
+		~audSFX();
+		int audLoadDir(string folder, string type = "");
+		int audChangeDir(string folder, string type = "");
+		int audPlay(int index);
+		int audPlay(string file);
 		int audStop();
 };
 
