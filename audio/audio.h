f81435482316cc025815e372dd1ae53845e02029
westonb2005 <westonb2005@333bce7a-0229-11df-9af4-1984060cff7e>
2010-01-25 06:06:57 +0000

    Changed tabs to 5 spaces

--- audio/audio.h
+++ audio/audio.h
@@ -8,38 +8,38 @@
 using namespace std;
 
 class audBackground {
-	private:
-		ALuint backgroundSource;
-		string currentFolder;	//Folder where background audio comes from
-		vector<string> files;	//Files to queue for background
-		int buffersize;		//Size of buffer queue
-		int currentFile;	//Index of files for current file
-		int audGetFilesOfType(string folder, string type);
-	public:
-		audBackground(int buffersize);
-		~audBackground();	//Cleanup
-		int audLoadDir(string folder, string type = "");
-		int audChangeDir(string folder, string type = "");
-		ALint audCheck();
-		int audUpdateQueue(int play);
-		int audPlay();
-		int audStop();
+     private:
+          ALuint backgroundSource;
+          string currentFolder;     //Folder where background audio comes from
+          vector<string> files;     //Files to queue for background
+          int buffersize;          //Size of buffer queue
+          int currentFile;     //Index of files for current file
+          int audGetFilesOfType(string folder, string type);
+     public:
+          audBackground(int buffersize);
+          ~audBackground();     //Cleanup
+          int audLoadDir(string folder, string type = "");
+          int audChangeDir(string folder, string type = "");
+          ALint audCheck();
+          int audUpdateQueue(int play);
+          int audPlay();
+          int audStop();
 };
 
 class audSFX {
-	private:
-		ALuint *sfxSource;
-		ALuint *sfxBuffer;
-		vector<string> files;
-		int audGetFilesOfType(string folder, string type);
-	public:
-		audSFX();
-		~audSFX();
-		int audLoadDir(string folder, string type = "");
-		int audChangeDir(string folder, string type = "");
-		int audPlay(int index);
-		int audPlay(string file);
-		int audStop();
+     private:
+          ALuint *sfxSource;
+          ALuint *sfxBuffer;
+          vector<string> files;
+          int audGetFilesOfType(string folder, string type);
+     public:
+          audSFX();
+          ~audSFX();
+          int audLoadDir(string folder, string type = "");
+          int audChangeDir(string folder, string type = "");
+          int audPlay(int index);
+          int audPlay(string file);
+          int audStop();
 };
 
 #endif
