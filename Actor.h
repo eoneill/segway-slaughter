370c416f76b6a403ec6482c86e929606a9d5440d
1JeffreyPrice <1JeffreyPrice@333bce7a-0229-11df-9af4-1984060cff7e>
2010-02-09 01:34:23 +0000

    Simple attack system working

--- Actor.h
+++ Actor.h
@@ -4,7 +4,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <Ogre.h>
+
 using namespace std;
+using namespace Ogre;
 
 const int DI_UP = 0;
 const int DI_DOWN = 1;
@@ -21,6 +24,10 @@ class Actor {
 		float MaxHealth;
 		float CurrentHealth;
 		float CollisionSideLength;
+		//float attackTime;
+		//float maxAttackTime;
+		bool attacking;
+		float damage;
 		virtual ~Actor(){};
 		//change this to take in the player and enemies by reference later
 		virtual bool move(int direction, vector<Actor*> actors){
@@ -73,7 +80,6 @@ class Actor {
 				
 				for(int j = 0; j < 4; j++)					
 				{
-					//cout << this_box[j][1] << " " << actor_box[0][1] << endl;
 					if(this_box[j][1] <= actor_box[0][1] &&
 						 this_box[j][1] >= actor_box[1][1] &&
 						 this_box[j][0] >= actor_box[0][0] &&
@@ -82,9 +88,83 @@ class Actor {
 						{
 							valid = false; 
 						}
-				}		
+				}
 			}
 			return valid;
 		}
+		
+		void attack(vector<Actor*> actors, Ogre::Root* root_, float damage){
+			//find out where the damage box is, based on direction facing
+			double damagePos[3];
+			damagePos[0] = position[0];
+			damagePos[1] = position[1];
+			damagePos[2] = position[2] + CollisionSideLength - (facingRight*2*CollisionSideLength);
+			
+			//x and z value for each point of the damage box
+			float damage_box[4][2];
+			damage_box[0][0] = damagePos[0] - CollisionSideLength/2;
+			damage_box[0][1] = damagePos[2] + CollisionSideLength/2;
+
+			damage_box[1][0] = damagePos[0] - CollisionSideLength/2;
+			damage_box[1][1] = damagePos[2] - CollisionSideLength/2;
+			
+			damage_box[2][0] = damagePos[0] + CollisionSideLength/2;
+			damage_box[2][1] = damagePos[2] + CollisionSideLength/2;
+			
+			damage_box[3][0] = damagePos[0] + CollisionSideLength/2;
+			damage_box[3][1] = damagePos[2] - CollisionSideLength/2;
+			
+			for(unsigned int i = 0; i < actors.size(); i++)
+			{
+				//get actor's hit box
+				float actor_box[4][2];
+				actor_box[0][0] = actors[i]->position[0] - actors[i]->CollisionSideLength/2;
+				actor_box[0][1] = actors[i]->position[2] + actors[i]->CollisionSideLength/2;
+
+				actor_box[1][0] = actors[i]->position[0] - actors[i]->CollisionSideLength/2;
+				actor_box[1][1] = actors[i]->position[2] - actors[i]->CollisionSideLength/2;
+				
+				actor_box[2][0] = actors[i]->position[0] + actors[i]->CollisionSideLength/2;
+				actor_box[2][1] = actors[i]->position[2] + actors[i]->CollisionSideLength/2;
+				
+				actor_box[3][0] = actors[i]->position[0] + actors[i]->CollisionSideLength/2;
+				actor_box[3][1] = actors[i]->position[2] - actors[i]->CollisionSideLength/2;
+				
+				for(int j = 0; j < 4; j++)					
+				{
+					if(damage_box[j][1] <= actor_box[0][1] &&
+						 damage_box[j][1] >= actor_box[1][1] &&
+						 damage_box[j][0] >= actor_box[0][0] &&
+						 damage_box[j][0] <= actor_box[2][0]
+						 )
+					{
+						//if we get here, the actor is hit by the attack
+						actors[i]->onDamage(damage, root_);
+					}
+				}
+			}
+		}
+		
+		//this might be a virtual function later
+		void onDamage(float damage, Ogre::Root* root_){
+			CurrentHealth -= damage;
+			if(CurrentHealth <= 0){
+				onDeath(root_);
+			}
+		}
+		
+		//this will be a virtual function later
+		void onDeath(Ogre::Root* root_)
+		{
+			//in leiu of actually destroying it, right now, we'll just move it.
+			position[1]+= 10000;
+			position[2]+= 10000;
+			//Ogre::Root* root_ = getRoot();
+			SceneNode* node = root_->getSceneManager("Default SceneManager")->getSceneNode(sceneNode);
+			node->translate(Vector3(0,10000,10000));
+		
+		}
 };
+
+
 #endif
