357d75f91ba4aaad91402f519c21a88fcbcbaca2
zenogais <zenogais@333bce7a-0229-11df-9af4-1984060cff7e>
2010-03-12 23:49:28 +0000

    Items now get unique identifiers.

--- Item.cpp
+++ Item.cpp
@@ -1,4 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+// Segway Slaughter
+//
+// Description:
+//   Various items seen throughout the game
+////////////////////////////////////////////////////////////////////////////////
 #include "Item.h"
+
+int Item::id_ = 0;
+
+////////////////////////////////////////////////////////////////////////////////
 
 Item::Item(const std::string& entityName,
            const std::string& entityMesh,
@@ -7,9 +17,16 @@ Item::Item(const std::string& entityName,
     sceneNode_(0),
     entity_(0)
 {
+	// Add a unique ID to the entity name
+	std::stringstream ss;
+	ss << entityName << id_++;
+	std::string entityNameUniq = ss.str();
+	
+	// Create an entity from the mesh
   Ogre::SceneManager* sceneMgr = Locator::getSceneManager();
-  entity_ = sceneMgr->createEntity(entityName, entityMesh);
+  entity_ = sceneMgr->createEntity(entityNameUniq, entityMesh);
 
+	// Attach to item to a scene node for rendering
   sceneNode_ = sceneMgr->getRootSceneNode()->createChildSceneNode(position_);
   sceneNode_->attachObject(entity_);
   sceneNode_->setScale(10, 10, 10);
