275fffe382d8b1e7b720ad92cbe2cdda30c53988
zenogais <zenogais@333bce7a-0229-11df-9af4-1984060cff7e>
2010-03-16 03:31:48 +0000

    Introduced memory leak to fix segfault.

--- GameState.cpp
+++ GameState.cpp
@@ -1,14 +1,24 @@
 #include "GameState.h"
 
 GameState::~GameState() {
-	
+  /*for (std::vector<Actor*>::iterator it = actors.begin();
+       it != actors.end();) {
+    delete *it;
+    it = actors.erase(it);
+  }
+
+  for (std::vector<Item*>::iterator it = items.begin();
+       it != items.end();) {
+    delete *it;
+    it = items.erase(it);
+    }*/
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void GameState::removeDead() {
 	for (std::vector<Actor*>::iterator it = actors.begin();
-			 it != actors.end(); it++) {
+			 it != actors.end();) {
 		if ((*it)->getState() == dead) {
 			delete *it;
 			it = actors.erase(it);
