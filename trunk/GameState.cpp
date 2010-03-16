385ffe7614d4f2d305f7689c4c20d1a8f7e73782
zenogais <zenogais@333bce7a-0229-11df-9af4-1984060cff7e>
2010-03-16 02:57:24 +0000

    Added GameState.cpp to makefile and project.

---
+++ trunk/GameState.cpp
@@ -0,0 +1,21 @@
+#include "GameState.h"
+
+GameState::~GameState() {
+	
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void GameState::removeDead() {
+	for (std::vector<Actor>*::iterator it = actors.begin();
+			 it != actors.end(); it++) {
+		if ((*it)->getState() == dead) {
+			delete *it;
+			actors.erase(it);
+		} else {
+			++it;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
